6fb8f9efbfdbeee0461abfa74f1d8e4fa1fd350e
Mike DeGasperis <mikedegasperis@hotmail.com>
2016-01-19 18:51:25 -0800

    Lab 3 - Print only crypto maps not using AES

--- week1/week1_lab3.py
+++ week1/week1_lab3.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+
 from ciscoconfparse import CiscoConfParse
 
 
@@ -31,8 +32,8 @@ def cmap_crypto(config):
 
 def pfs_group2(config):
     '''
-    Accepts CiscoConfParse object consisting of a list
-    of crypto map entries in a cisco config file.
+    Accepts Cisco configuration and finds crypto map
+    entries using only pfs group 2.
     '''
     
 
@@ -40,17 +41,44 @@ def pfs_group2(config):
     cisco_cfg = CiscoConfParse(config)
     
     
-    # Find only crypto maps using pfs group 2
-    pfs2 = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
+    # Find only crypto maps using pfs group 2.
+    cmap = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
 
     
     # Iterate over lines matched previously. Print the line. 
-    for seq_num in pfs2:
+    for seq_num in cmap:
         print seq_num.text
     
         # Iterate over children of the config line, print children. 
         for child in seq_num.children:
                 print child.text
+
+
+
+
+def no_aes(config):
+    '''
+    Accepts Cisco configuration and finds crypto map
+    entries not using AES transform sets.
+    '''
+
+
+    # Create CiscoConfParse object with config passed into function.
+    cisco_cfg = CiscoConfParse(config)
+    
+    # Find only crypto maps not using AES transform set.
+    cmap = cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r".+ AES")
+
+
+    # Iterate over lines matched previously. Print the line.
+    for seq_num in cmap:
+        print seq_num.text
+
+        # Iterate over children of the config line, print children. 
+        for child in seq_num.children:
+            print child.text
+
+
 
 
 def main():
@@ -65,12 +93,21 @@ def main():
 
     # Call function to print and return all crypto maps.
     print "\n\n---------- All Crypto Maps ----------\n"
-    crypto_map = cmap_crypto(config)
+    cmap_crypto(config)
     
 
     # Call function to return only crypto maps using pfs group 2.
     print "\n\n------------ Only PFS 2 ------------\n"
-    pfs_2 = pfs_group2(config)
+    pfs_group2(config)
+
+
+    # Call function to return only crypto maps not using AES.
+    print "\n\n------------- No AES -------------\n"
+    no_aes(config)
+
+    print
+    print
+
 
 
 
