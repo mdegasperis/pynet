72e1beb13127789f2621d6798bcd30d339844ce0
Mike DeGasperis <mikedegasperis@hotmail.com>
2016-01-19 18:35:02 -0800

    Lab 3 - Print only crypto maps using pfs group 2

--- week1/week1_lab3.py
+++ week1/week1_lab3.py
@@ -9,6 +9,7 @@ def cmap_crypto(config):
     titled CRYPTO.
     '''
 
+
     # Create CiscoConfParse object with config passed into function.
     cisco_cfg = CiscoConfParse(config)
     
@@ -25,7 +26,31 @@ def cmap_crypto(config):
         for child in seq_num.children:
             print child.text
 
+
+
+
+def pfs_group2(config):
+    '''
+    Accepts CiscoConfParse object consisting of a list
+    of crypto map entries in a cisco config file.
+    '''
     
+
+    # Create CiscoConfParse object with config passed into function.
+    cisco_cfg = CiscoConfParse(config)
+    
+    
+    # Find only crypto maps using pfs group 2
+    pfs2 = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
+
+    
+    # Iterate over lines matched previously. Print the line. 
+    for seq_num in pfs2:
+        print seq_num.text
+    
+        # Iterate over children of the config line, print children. 
+        for child in seq_num.children:
+                print child.text
 
 
 def main():
@@ -33,12 +58,20 @@ def main():
     Main Program
     '''
 
+
     # Create config file object.
     config = "cisco_ipsec.txt"
+
+
+    # Call function to print and return all crypto maps.
+    print "\n\n---------- All Crypto Maps ----------\n"
+    crypto_map = cmap_crypto(config)
     
-    # Call function to print all crypto maps.
-    cmap_crypto(config)
-    
+
+    # Call function to return only crypto maps using pfs group 2.
+    print "\n\n------------ Only PFS 2 ------------\n"
+    pfs_2 = pfs_group2(config)
+
 
 
 if __name__ == '__main__':
