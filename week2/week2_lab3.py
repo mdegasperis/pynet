c06a42b1e9510fb099255a92d709ca13906ccc6d
Mike DeGasperis <mikedegasperis@hotmail.com>
2016-01-26 13:48:59 -0800

    Lab 3 - Test Class Inheritance

--- week2/week2_lab3.py
+++ week2/week2_lab3.py
@@ -9,8 +9,35 @@ import getpass
 
 # Convert Telnet code created by Kirk from using functions to classes.
 
-class Telnet(object):
-    def __init__(self, ip, username, password, port, timeout, cmd):
+
+class TelnetConnection(object):
+    def __init__(self, ip, username, password, port, timeout):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.port = port
+        self.timeout = timeout
+    
+    def login(self):
+        try:
+            self.remote_conn = telnetlib.Telnet(self.ip, self.port, self.timeout)
+        except socket.timeout:
+            sys.exit("Connection timed out")
+
+        # Authenticate
+        output = self.remote_conn.read_until("sername:", self.timeout)
+        self.remote_conn.write(self.username + "\n")
+        output = self.remote_conn.read_until("assword:", self.timeout)
+        self.remote_conn.write(self.password + "\n")
+        output = self.remote_conn.read_very_eager()
+
+        # Disable paging
+        self.remote_conn.write("terminal length 0\n")
+        output = self.remote_conn.read_very_eager()
+        return output
+
+class Telnet(TelnetConnection):
+    def __init__(self, cmd='', ip='', username='', password='', port='', timeout=''):
         self.ip = ip
         self.username = username
         self.password = password
@@ -18,41 +45,33 @@ class Telnet(object):
         self.timeout = timeout
         self.cmd = cmd
 
-    def connect(self):
-        # Establish connection to network device
-        try:
-            remote_conn = telnetlib.Telnet(self.ip, self.port, self.timeout)
-        except socket.timeout:
-            sys.exit("Connection timed out")
+        # TelnetConnection.__init__(self, ip, username, password, port, timeout)        
+        self.remote_conn = TelnetConnection(self.ip, self.username, self.password, self.port, self.timeout)
         
-        # Authenticate
-        output = remote_conn.read_until("sername:", self.timeout)
-        remote_conn.write(self.username + "\n")
-        output = remote_conn.read_until("assword:", self.timeout)
-        remote_conn.write(self.password + "\n")
-        output = remote_conn.read_very_eager()
 
-        # Disable paging
-        remote_conn.write("terminal length 0\n")
-        output = remote_conn.read_very_eager()
-        return output
+        self.remote_conn.login()
+        #return self.remote_conn
 
-
-
+    def send(self, cmd):
+        #cmd = self.cmd
+        cmd = cmd.rstrip()
+        self.remote_conn.remote_conn.write(cmd + '\n')
+        time.sleep(1)
+        return self.remote_conn.remote_conn.read_very_eager()
 
 
 
 def main():
-    TELNET_PORT = 23
-    TELNET_TIMEOUT = 6
-    ip_addr = '50.76.53.27'
+    port = 23
+    timeout = 6
+    ip = '50.76.53.27'
     username = 'pyclass'
     password = '88newclass'
     command = 'show version'
 
-    remote_conn = Telnet(ip_addr, username, password, TELNET_PORT, TELNET_TIMEOUT, command)
+    remote_conn = Telnet(command, ip, username, password, port, timeout)
     
-    output = remote_conn.connect()
+    output = remote_conn.send('show ip int brief')
     print output
 
 if __name__ == '__main__':
